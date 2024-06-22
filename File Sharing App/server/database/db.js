152ee8f7cf2f7a2f9eb64efe05fb6ea472b5ed44
Priyanshu Kapil <133043712+Priyanshukapil001@users.noreply.github.com>
2024-06-22 20:40:24 +0530

    Update db.js

--- File Sharing App/server/database/db.js
+++ File Sharing App/server/database/db.js
@@ -7,7 +7,7 @@ const DBConnection = async () => {
     const USERNAME = process.env.DB_USERNAME;
     const PASSWORD = process.env.DB_PASSWORD;
 
-    const MONGO_URI = `mongodb+srv://${USERNAME}:${PASSWORD}@filesharing.ddtkr5c.mongodb.net/`;
+    const MONGO_URI = `mongodb+srv://${USERNAME}:${PASSWORD}@filesharingappp.s2kbigc.mongodb.net/?retryWrites=true&w=majority&appName=FileSharingAppp`;
     try {
         await mongoose.connect(MONGO_URI);
         console.log('Database connected successfully');
@@ -17,3 +17,4 @@ const DBConnection = async () => {
 }
 
 export default DBConnection;
+
